5b75875652b37639dea7a7c7bd8277d6c044a27b
ilguarnieri <guarnieriangelo91ag@gmail.com>
2022-06-23 15:20:13 +0200

    system number array

--- Program.cs
+++ Program.cs
@@ -14,10 +14,21 @@ int lungArray = cibi_preferiti.Length;
 
 Console.WriteLine($"La tua classifica contiene {lungArray} cibi:");
 
+int k = 1;
 for(int i = 0; i < lungArray; i++)
 {
-    Console.WriteLine($"{i} {cibi_preferiti[i]}");
+    Console.WriteLine($"{k} {cibi_preferiti[i]}");
+    k++;
 }
 
 Console.WriteLine($"Il tuo cibo preferito è: {cibi_preferiti[0]}");
 Console.WriteLine($"Il tuo cibo preferito ma non troppo è: { cibi_preferiti[lungArray - 1]}");
+
+if(lungArray % 2 == 0)
+{
+    Console.WriteLine("pari");
+}
+else
+{
+    Console.WriteLine("dispari");
+}
